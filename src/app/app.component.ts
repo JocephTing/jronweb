6adafb4089d20f7a0ea72b09213080d1c69689bd
joseph ting <joseph.ting@nextfortune.tw>
2022-11-02 14:45:46 +0800

    add tronweb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var tronWeb: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jronweb';
+
+  getTronWeb() {
+    if(tronWeb && tronWeb.defaultAddress.base58){
+      console.log("Yes, catch it:", tronWeb.defaultAddress.base58);
+      tronWeb.trx.getBalance(tronWeb.defaultAddress.base58).then((result: any) => {
+        console.log("and your balance is :", result)
+      })
+    }
+    else {
+      console.log("fucked up");
+    }
+  }
+
+  async signSomething() {
+    var bla = "foo bar"
+    var blahex = tronWeb.toHex(bla);
+    var signature = await tronWeb.trx.sign(blahex);
+    console.log(signature)
+  }
+
+  async interactWithContract() {
+    let contract = await tronWeb.contract().at("TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs");
+    try {
+      let res = await contract.transfer('TQHMWoWU8dewkYMzG2xpdybuDqoXZqFe39', 5000000).send({
+        feeLimit:100_000,
+        callValue:0,
+        shouldPollResponse:true
+      });
+      console.log(res);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 }
